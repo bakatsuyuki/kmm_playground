df2925c2bac393d0367d92ca951bd756b58037f8
bakatsuyuki <bakatsuyuki@icloud.com>
2024-04-29 18:47:48 +0900

    clean: inject client to repository

--- shared/src/commonMain/kotlin/com/example/kmm_playground/Koin.kt
+++ shared/src/commonMain/kotlin/com/example/kmm_playground/Koin.kt
@@ -1,9 +1,12 @@
 package com.example.kmm_playground
 
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
-import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.bind
 import org.koin.dsl.module
 
 fun commonStartKoin() = startKoin {
@@ -13,5 +16,14 @@ fun commonStartKoin() = startKoin {
 expect fun platformModule(): Module
 
 private val commonModule = module {
-    singleOf(::TodosRepository) bind TodosRepository::class
+    single {
+        HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                })
+            }
+        }
+    }
+    single { TodosRepository(get()) }
 }
--- shared/src/commonMain/kotlin/com/example/kmm_playground/TodosRepository.kt
+++ shared/src/commonMain/kotlin/com/example/kmm_playground/TodosRepository.kt
@@ -2,23 +2,11 @@ package com.example.kmm_playground
 
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
-import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.serialization.json.Json
 
-class TodosRepository {
-    private val client = HttpClient(CIO) {
-        install(ContentNegotiation) {
-            json(Json {
-                ignoreUnknownKeys = true
-            })
-        }
-    }
-
+class TodosRepository(private val client: HttpClient) {
     fun listTodos(): Flow<List<Todo>> = flow {
         val response = client.get("$BASE_URL/users/1/todos")
         val jsonArray = response.body<List<Todo>>()
